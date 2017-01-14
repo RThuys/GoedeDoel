11dae9fb096434cefdf5f1e4c4803e9c9f0aad2c
RolandT <rolandthuys@gmail.com>
2017-01-14 17:39:14 +0100

    test voor automation

--- goededoel/src/main/java/be/vdab/servlets/IndexServlet.java
+++ goededoel/src/main/java/be/vdab/servlets/IndexServlet.java
@@ -15,6 +15,7 @@ public class IndexServlet extends HttpServlet {
 	private static final String VIEW="/WEB-INF/JSP/index.jsp";
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("doel", new GoedeDoel("CliniClowns"));
+		System.out.println("change");
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
